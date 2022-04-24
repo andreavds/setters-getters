250cb7ee83ce639638beb388e409efd4f29d2bf4
andreavds <98629913+andreavds@users.noreply.github.com>
2022-04-24 03:34:50 -0400

    Add files via upload

---
+++ Vehiculos/src/Uso_Moto.java
@@ -0,0 +1,13 @@
+
+public class Usu_Moto {
+
+        public static void main (String[] args) {
+               
+                Moto Turbo= new Moto();
+               
+                 Turbo.ruedas;
+
+        System.out.println(Turbo.dimeLargo() + Turbo.dimeRuedas());
+               
+        }
+}
